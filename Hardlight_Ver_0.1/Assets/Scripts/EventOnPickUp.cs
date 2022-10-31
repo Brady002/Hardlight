be51f8eef1ddbfb288dc3c1a9c025f9f6a2cba37
Brady002 <bradywright@u.boisestate.edu>
2022-10-31 17:09:41 -0600

    Turret Room
    Added the first turret room

---
+++ Hardlight_Ver_0.1/Assets/Scripts/EventOnPickUp.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventOnPickUp : MonoBehaviour
+{
+    public GameObject lockedDoor;
+    private Animator doorAnim; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAnim = lockedDoor.transform.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy();
+        doorAnim.SetBool("IsOpening", true);
+    }
+}
